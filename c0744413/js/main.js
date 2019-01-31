a8ace2c7cb0c65aca0d9ad531a183b46373e8cdc
jaspreet kaur <jaspreetsidhu901@gmail.com>
2019-01-31 11:44:49 -0500

    comment

--- c0744413/js/main.js
+++ c0744413/js/main.js
@@ -1,1 +1,31 @@
-﻿// Your code here!
+﻿alert("I love ducks");
+
+alert(2 + 2);
+
+var date = new Date();
+
+var date = new Date();
+alert(date);
+
+console.log("The date is" + date);
+
+var dog = new Date();
+
+var dog = new Date();
+alert(dog);
+
+document.body.innerHTML = "<h1> The date is " + dog + "</h1>";
+
+var cat = new Date();
+alert(cat);
+
+alert(cat = dog);
+
+document.body.innerHTML = "<h1> The date is " + dog.getMonth() + "/" + dog.getDate() + "/" + dog.getFullYear() + "</h1>";
+
+document.body.innerHTML = "<h1> The date is " + (dog.getMonth() + 1) + "/" + dog.getDate() + "/" + dog.getFullYear() + "</h1>";
+
+function myFunction() {
+    var cat = new Date();
+    document.body.innerHTML = "<h1> The date is " + (dog.getMonth() + 1) + "/" + dog.getDate() + "/" + dog.getFullYear() + "</h1>";
+}
